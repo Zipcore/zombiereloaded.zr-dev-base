ea588d6242d4cacda885356b562dfb490c23e3f0
Richard Helgeby <Richard Helgeby@localhost>
2011-03-30 19:59:08 +0200

    Updated parts of the base to project-dev:74011ffb1241 (93). Updated compiler and includes to 1.4.0-3218. Not changed in project.inc because of compatibility: PROJECT_SERVER_PREFIX PROJECT_CVAR_PREFIX PROJECT_CMD_PREFIX TM_MSGFORMAT_* LM_FORMAT

--- compile.bat
+++ compile.bat
@@ -1,9 +1,13 @@
 @echo off
 
+:: Update these with your project's name.
+set SOURCEFILE=src\zombiereloaded.sp
+set PLUGIN=zombiereloaded.smx
+
 set SOURCEDIR=src
 set SMINCLUDES=env\include
 set BUILDDIR=build
-set SPCOMP=env\win32\bin\spcomp-1.4.0-3078.exe
+set SPCOMP=env\win32\bin\spcomp-1.4.0-3218.exe
 set VERSIONDUMP=updateversion.bat
 
 :: Dump version and revision information first.
@@ -17,7 +21,7 @@ if not exist "%BUILDDIR%" (
 
 :: Compile.
 echo Starting compiler:
-%SPCOMP% -i%SOURCEDIR% -i%SOURCEDIR%/include -i%SMINCLUDES% -o%BUILDDIR%/zombiereloaded.smx %SOURCEDIR%\zombiereloaded.sp
+%SPCOMP% -i%SOURCEDIR% -i%SMINCLUDES% -o%BUILDDIR%/%PLUGIN% %SOURCEFILE%
 
 echo Compiling done. This script is looped, close if you're done.
 pause

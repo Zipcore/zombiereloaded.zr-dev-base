05c493e01d2055a25614b78f500abe8272bc38e3
Richard Helgeby <Richard Helgeby@localhost>
2011-04-26 22:47:20 +0200

    More work on models module. * Added model collections (with loading and validation) * Added some model manager API functions. * Added model authorization API (with cache for lookup performance). * Still doesn't precache models yet.

--- codesummary.sh
+++ codesummary.sh
@@ -3,34 +3,39 @@
 cd src/
 
 # General and individual files.
-LINES_MAIN=`wc *.sp -l | cut -d ' ' -f1`
-LINES_BASE=`wc zr/base/*.* -l | tail -n1 | sed 's/^ *\(.*\) *$/\1/' | cut -d ' ' -f1`
-LINES_LIBRARIES=`wc zr/libraries/*.* -l | tail -n1 | sed 's/^ *\(.*\) *$/\1/' | cut -d ' ' -f1`
-LINES_MODULES=`wc zr/modules/*.* -l | tail -n1 | sed 's/^ *\(.*\) *$/\1/' | cut -d ' ' -f1`
-LINES_OTHER=`wc zr/*.* -l | tail -n1 | sed 's/^ *\(.*\) *$/\1/' | cut -d ' ' -f1`
+LINES_MAIN=`wc -l *.sp | tail -n1 | sed 's/^ *\(.*\) *$/\1/' | cut -d ' ' -f1`
+LINES_BASE=`wc -l zr/base/*.* | tail -n1 | sed 's/^ *\(.*\) *$/\1/' | cut -d ' ' -f1`
+LINES_LIBRARIES=`wc -l zr/libraries/*.* | tail -n1 | sed 's/^ *\(.*\) *$/\1/' | cut -d ' ' -f1`
+LINES_MODULES=`wc -l zr/modules/*.* | tail -n1 | sed 's/^ *\(.*\) *$/\1/' | cut -d ' ' -f1`
+LINES_OTHER=`wc -l zr/*.* | tail -n1 | sed 's/^ *\(.*\) *$/\1/' | cut -d ' ' -f1`
 
 # Cores
-LINES_ZRC_CORE=`wc zr/modules/zrc_core/*.* -l | tail -n1 | sed 's/^ *\(.*\) *$/\1/' | cut -d ' ' -f1`
-LINES_ZRIOT_CORE=`wc zr/modules/zriot_core/*.* -l | tail -n1 | sed 's/^ *\(.*\) *$/\1/' | cut -d ' ' -f1`
+LINES_ZRC_CORE=`wc -l zr/modules/zrc_core/*.* | tail -n1 | sed 's/^ *\(.*\) *$/\1/' | cut -d ' ' -f1`
+LINES_ZRIOT_CORE=`wc -l zr/modules/zriot_core/*.* | tail -n1 | sed 's/^ *\(.*\) *$/\1/' | cut -d ' ' -f1`
 
-# Modules
-LINES_WEAPONS=`wc zr/modules/weapons/*.* -l | tail -n1 | sed 's/^ *\(.*\) *$/\1/' | cut -d ' ' -f1`
+# Larger modules
+LINES_WEAPONS=`wc -l zr/modules/weapons/*.* | tail -n1 | sed 's/^ *\(.*\) *$/\1/' | cut -d ' ' -f1`
+LINES_MODELS=`wc -l zr/modules/models/*.* | tail -n1 | sed 's/^ *\(.*\) *$/\1/' | cut -d ' ' -f1`
 
+# Totals
+GENERAL_TOTAL="$(($LINES_MAIN + $LINES_BASE + $LINES_LIBRARIES + $LINES_OTHER))"
+CORES_TOTAL="$(($LINES_ZRC_CORE + LINES_ZRIOT_CORE))"
+MODULES_TOTAL="$(($LINES_WEAPONS + $LINES_MODELS + $LINES_MODULES))"
 
-
-LINES_TOTAL="$(($LINES_MAIN + $LINES_BASE + $LINES_LIBRARIES + $LINES_MODULES + $LINES_OTHER + $LINES_ZRC_CORE + $LINES_ZRIOT_CORE + $LINES_WEAPONS))"
+LINES_TOTAL="$(($GENERAL_TOTAL + $CORES_TOTAL + $MODULES_TOTAL))"
 
 echo "Number of lines:"
 echo "$LINES_MAIN\tmain sp"
 echo "$LINES_BASE\tbase"
 echo "$LINES_LIBRARIES\tlibraries"
-echo "$LINES_MODULES\tmodules"
-echo "$LINES_OTHER\tother"
+echo "$LINES_OTHER\tother (config, events)"
 echo ""
 echo "$LINES_ZRC_CORE\tzrc_core"
 echo "$LINES_ZRIOT_CORE\tzriot_core"
 echo ""
+echo "$LINES_MODELS\tmodels"
 echo "$LINES_WEAPONS\tweapons"
+echo "$LINES_MODULES\tother modules"
 
 echo "\nTotal:"
 echo "$LINES_TOTAL"
